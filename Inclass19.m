03f6357d37034461b24e913b24039a3dc2a7875d
xiangyukong <31257615+xiangyukong@users.noreply.github.com>
2017-11-06 11:16:10 -0600

    Add files via upload

--- Inclass19.m
+++ Inclass19.m
@@ -12,10 +12,27 @@ treatment2 =[   152   152   151   143   161   142   142   141   161   135];
 
 %A. Make a bar plot of the means of each group with errorbars (Hint, use
 %bar and then use 'hold on' and then errorbar). 
+
+y = [mean(placebo) mean(treatment1) mean(treatment2)];
+x = categorical({'placebo','treatment1','treatment2'});
+bar(x,y);
+err = [std(placebo) std(treatment1) std(treatment2)];
+hold on;
+errorbar(x,y,err)
+
 %B. Run a one way ANOVA analysis to see whether any of the treatment groups showed
-%a statistically signficant difference. 
+%a statistically signficant difference.
+
+data = [placebo treatment1 treatment2];
+groups = {'placebo','placebo','placebo','placebo','placebo','placebo','placebo','placebo','placebo','placebo','treatment1','treatment1','treatment1','treatment1','treatment1','treatment1','treatment1','treatment1','treatment1','treatment1','treatment2','treatment2','treatment2','treatment2','treatment2','treatment2','treatment2','treatment2','treatment2','treatment2'};
+[p,table,stats] = anova1(data, groups);
+%is a significant difference
+
 %C. use the multcompare function to look at all pairs of samples and
 %indicate which are significantly different. 
+
+multcompare(stats)
+%placebo and treatment1 are significatly different from treatment2
 
 %Problem 2. In this directory, you will find a .mat file with three
 %variables, xdat, ydat, and ydat2. For each pair (xdat, ydat) and (xdat,
@@ -23,6 +40,31 @@ treatment2 =[   152   152   151   143   161   142   142   141   161   135];
 %one is the best fit in each case? In at least one of the cases, do the
 %problem with both the polyfit/polyval functions and with the 'fit' function.
 
-
-
-
+load('data.mat');
+xdat = [xdat]';
+ydat = [ydat]';
+ydat2 = [ydat2]';
+fit_1_1 = fit(xdat,ydat,'poly1');
+fit_1_2 = fit(xdat,ydat,'poly2');
+fit_1_3 = fit(xdat,ydat,'poly3');
+fit_2_1 = fit(xdat,ydat2,'poly1');
+fit_2_2 = fit(xdat,ydat2,'poly2');
+fit_2_3 = fit(xdat,ydat2,'poly3');
+figure;hold on;
+plot(xdat,ydat,'r.','MarkerSize',18);hold on;
+plot(xdat,fit_1_1(xdat),'g-','LineWidth',3);
+plot(xdat,fit_1_2(xdat),'b-','LineWidth',3);
+plot(xdat,fit_1_3(xdat),'k-','LineWidth',3);
+%first order fits just fine, other ones should be overfitting
+plot(xdat,ydat2,'r.','MarkerSize',18);hold on;
+plot(xdat,fit_2_1(xdat),'g-','LineWidth',3);
+plot(xdat,fit_2_2(xdat),'b-','LineWidth',3);
+plot(xdat,fit_2_3(xdat),'k-','LineWidth',3);
+%third order fits best
+[coeff1,s1] = polyfit(xdat,ydat2,1);
+[coeff2,s2] = polyfit(xdat,ydat2,2);
+[coeff3,s3] = polyfit(xdat,ydat2,3);
+plot(xdat,ydat2,'r.','MarkerSize',18);hold on;
+plot(xdat,polyval(coeff1,xdat),'k-','LineWidth',3);
+plot(xdat,polyval(coeff2,xdat),'g-','LineWidth',3);
+plot(xdat,polyval(coeff3,xdat),'b-','LineWidth',3);
